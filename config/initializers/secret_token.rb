0aa28f0afbcc24f705a42f0c3ee9ffc6bb0d385c
coln <coln@live.com>
2012-03-07 20:24:30 +0800

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nownexter::Application.config.secret_token = '700697ff7d5901a1496163a43e321bfb5917ef9f3d9b5e8fac54c878d60777f7defdd7bc27d92260364fa2bb5a208292a61fc73c92c7fbfc6ca84f9ccea55afe'
